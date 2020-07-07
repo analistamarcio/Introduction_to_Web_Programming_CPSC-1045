15a424f521bbb74a2b9561eac720fe8f5c560941
Marcio Ferreira <analistamarcio@hotmail.com>
2020-07-07 09:13:44 -0700

    Lab3 completed

--- Week2/Lab2/exercises.js
+++ Week2/Lab2/exercises.js
@@ -3,11 +3,22 @@
 //   let num2 = +prompt("Enter the second number");
 //   document.getElementById("result").innerHTML = Math.pow(num1, 3) + Math.pow(num2, 3);
 // }
-function capStr() {
-  let userStr = prompt("Enter a text");
-  showResult(userStr);
-}
+// function capStr() {
+//   let userStr = prompt("Enter a text");
+//   showResult(userStr);
+// }
 
-function showResult(str) {
-  document.getElementById("result").innerHTML = str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+// function showResult(str) {
+//   document.getElementById("result").innerHTML = str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+// }
+
+function myFunc() {
+  let result = "";
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < i; j++) {
+      result += "*";
+    }
+    result += "<br>"
+  }
+  document.getElementById("myDiv").innerHTML = result;
 }
